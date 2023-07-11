b9063b8127b679d136ff1913086d4ee6ba709f72
Lakshya0257 <lakshyabhati24@gmail.com>
2023-07-12 00:20:41 +0530

    loops

---
+++ loops/loops.rs
@@ -0,0 +1,11 @@
+fn main() {
+    //starting and ending range
+    for i in -3..=2 {
+        println!("{}", i);
+    }
+
+    //printing the decimal values for all character
+    for c in 'a'..='z' {
+        println!("{}", c as u16);
+    }
+}
